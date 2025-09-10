68888e3690513d5940b4f3af749fca604f58ea13
an07k <anil.kurt7@hotmail.com>
2025-09-10 20:44:10 +0300

    v0.15.1 Trying Camera

--- Assets/_GameAssets/Scripts/Gameplay/Camera/TPPCameraController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Camera/TPPCameraController.cs
@@ -1,31 +1,25 @@
 using UnityEngine;
-
 public class TPPCameraController : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private Transform _orientationTransform;
     [SerializeField] private Transform _playerVisualTransform;
-
     [Header("Settings")]
     [SerializeField] private float _rotationSpeed;
     private void Update()
     {
-        if (GameManager.Instance.GetCurrentGameState() != GameState.Play && GameManager.Instance.GetCurrentGameState() != GameState.Resume)
+        if
+        (GameManager.Instance.GetCurrentGameState() != GameState.Play
+        && GameManager.Instance.GetCurrentGameState() != GameState.Resume)
         {
             return;
         }
-        Vector3 viewDirection =
-            _playerTransform.position - new Vector3(transform.position.x, _playerTransform.position.y, transform.position.z);
-
+        Vector3 viewDirection = _playerTransform.position - new Vector3(transform.position.x, _playerTransform.position.y, transform.position.z);
         _orientationTransform.forward = viewDirection.normalized;
-
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
-
-        Vector3 inputDirection =
-        _orientationTransform.forward * verticalInput + _orientationTransform.right * horizontalInput;
-
+        Vector3 inputDirection = _orientationTransform.forward * verticalInput + _orientationTransform.right * horizontalInput;
         if (inputDirection != Vector3.zero)
         {
             _playerVisualTransform.forward =
